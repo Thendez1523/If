9807610a46ce32ec60952a75b4c425116f783ca2
Thendez1523 <102190814+Thendez1523@users.noreply.github.com>
2022-04-02 13:17:29 -0600

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a;
+    cout<<"Ingrese un valor:"<<endl;
+    cin>> a;
+     if (a>50)
+     {
+      cout<<"El Valor es positivo"<<endl;
+     }
+     else
+     {
+         cout<<"El Valor es negativo"<<endl;
+     }
+}
